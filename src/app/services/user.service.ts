ca8074b94f6dc5c58c45299c6b75e7159267889d
andrelup <andrelcop@gmail.com>
2022-10-16 15:33:52 +0200

    clean code

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,20 +5,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root',
 })
 export class UserService {
-  url = 'localhost:3000/users/';
+  url = 'http://localhost:3000/users/';
   constructor(private http: HttpClient) {}
   login(email: string, password: string) {
     let body = { email: email, password: password };
-    return this.http.post(this.url + 'login', body, HTTP_OPTIONS);
+    return this.http.post(this.url + 'login', body);
   }
 }
-export const HTTP_OPTIONS = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, PUT, OPTIONS',
-    'Access-Control-Allow-Headers':
-      'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
-  }),
-};
